5bacf2cd33b74b8c90ad3f4ce01179d55a936e27
vova836 <123488703+vova836@users.noreply.github.com>
2024-02-12 15:47:23 +0200

    Add files via upload

---
+++ Schoolchild.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Theme19;
+class Schoolchild
+{
+    public string FullName { get; set; }
+    public string Gender { get; set; }
+    public int YearOfBirth { get; set; }
+
+    public Schoolchild(string fullName, string gender, int yearOfBirth)
+    {
+        FullName = fullName;
+        Gender = gender;
+        YearOfBirth = yearOfBirth;
+    }
+}
+
